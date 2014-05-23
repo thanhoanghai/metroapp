66520c6fe21bc7bb78497d351a901dc6081bb145
thanhoanghai <thanhoanghai@gmail.com>
2014-05-24 00:25:06 +0700

    * update code

--- IOS/metro/metro/MTViewController.h
+++ IOS/metro/metro/MTViewController.h
@@ -13,12 +13,14 @@
 @interface MTViewController : UIViewController<DialogDelegate>
 {
     NSTimer *nsTimerUp;
+    
     int countTime;
-    NganhListObject *nganhListObject;
-    NganhListObject *metroListObject;
     int indexDialog;
     int indexBranchMetro;
     int indexNganh;
+
+    NganhListObject *nganhListObject;
+    NganhListObject *metroListObject;
     
     DialogController *dialogView;
 }
@@ -26,15 +28,14 @@
 
 
 @property (weak, nonatomic) IBOutlet UIButton *bntChon;
-
-
 @property (weak, nonatomic) IBOutlet UIView *viewDisconect;
 @property (weak, nonatomic) IBOutlet UIView *viewContent;
 @property (weak, nonatomic) IBOutlet UIButton *lbBranchMetro;
 @property (weak, nonatomic) IBOutlet UIButton *lbNganh;
-
 @property (weak, nonatomic) IBOutlet UIImageView *imgLogo;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loading;
+
+
 - (IBAction)doActionBntChon:(id)sender;
 - (IBAction)doActionBntBranchMetro:(id)sender;
 - (IBAction)doActionBntNganh:(id)sender;
